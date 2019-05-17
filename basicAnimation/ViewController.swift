56d728de6d2e204eb5efc8eee9118e107fdab48b
Muhammad Rajab <m.rajab.priharsanto@gmail.com>
2019-05-17 13:35:41 +0700

    Initial Commit

---
+++ basicAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  basicAnimation
+//
+//  Created by Muhammad Rajab Priharsanto on 17/05/19.
+//  Copyright © 2019 Muhammad Rajab Priharsanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
