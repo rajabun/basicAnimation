171858b837b233a015944b3434104d8222e792d2
Muhammad Rajab <m.rajab.priharsanto@gmail.com>
2019-05-17 20:08:19 +0700

    Finish 6 Basic Animations (Frame, Bounds, Center, Alpha, Background Color & Transform)

--- basicAnimation/ViewController.swift
+++ basicAnimation/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController
 
     //#1. Deklarasi Button
     @IBOutlet weak var myObject: UIButton!
+    @IBOutlet weak var myObjectBounds: UIButton!
+    @IBOutlet weak var myObjectCenter: UIButton!
+    @IBOutlet weak var myObjectAlpha: UIButton!
+    @IBOutlet weak var myObjectBackgroundColor: UIButton!
+    @IBOutlet weak var myObjectTransform: UIButton!
+    
     
     override func viewDidLoad()
     {
@@ -44,5 +50,52 @@ class ViewController: UIViewController
         }
     }
 
+    @IBAction func boundsButton(_ sender: UIButton)
+    {
+        UIView.animate(withDuration: 1)
+        {
+            self.myObjectBounds.bounds = CGRect(x: 23, y: 100, width: 100, height: 100)
+            print(self.myObjectBounds.bounds)
+        }
+    }
+    
+    @IBAction func centerButton(_ sender: UIButton)
+    {
+        UIView.animate(withDuration: 1)
+        {
+            self.myObjectCenter.center = CGPoint(x: 50, y: 50)
+            print(self.myObjectCenter.center)
+        }
+    }
+    
+    @IBAction func alphaButton(_ sender: UIButton)
+    {
+        UIView.animate(withDuration: 1)
+        {
+            self.myObjectAlpha.alpha = CGFloat(bitPattern: UInt(0.5))
+            print(self.myObjectAlpha.alpha)
+        }
+    }
+    
+    @IBAction func backgroundColorButton(_ sender: UIButton)
+    {
+        UIView.animate(withDuration: 1)
+        {
+            self.myObjectBackgroundColor.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+            self.view.backgroundColor = .black
+        }
+    }
+    
+    @IBAction func transformButton(_ sender: UIButton)
+    {
+        UIView.animate(withDuration: 1)
+        {
+            self.myObjectTransform.transform = CGAffineTransform(rotationAngle: 45)
+            self.myObjectTransform.transform = CGAffineTransform(scaleX: 5, y: 5)
+            self.myObjectTransform.transform = CGAffineTransform(translationX: 40, y: 60)
+            
+        }
+    }
+  
 }
 
